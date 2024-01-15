7271da0ef269ee63cc21de9ed9fa773f557dddc4
AhmedHome <ahmedHome@Numus-MacBook-Pro.local>
2024-01-15 19:11:05 +0500

    test api

--- broker.js
+++ broker.js
@@ -1,8 +1,17 @@
 // MQTT broker
-var mosca = require("mosca");
+const mosca = require("mosca");
+const express = require("express");
+const app = express();
+
 var settings = { port: 1234 };
 var broker = new mosca.Server(settings);
+
+app.get("/test", (req, res) => res.send("app is working"));
+
+const port = 8080;
 
 broker.on("ready", () => {
   console.log("Broker is ready!");
 });
+
+app.listen(port, () => console.log("live at ", port));
