26877b9275f0e917ebaceb27e864017eb861228e
AhmedHome <ahmedHome@Numus-MacBook-Pro.local>
2024-01-16 20:20:14 +0500

    main rename

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const app = express();
 var settings = { port: 1234, host: "0.0.0.0" };
 var broker = new mosca.Server(settings);
 
-app.get("/test", (req, res) => res.send("app is working"));
+app.get("/test", (req, res) => res.send("app is working!"));
 
 const port = 8080;
 
 broker.on("ready", () => {
-  console.log("Broker is ready!");
+  console.log("!Broker is ready!");
 });
 
 app.listen(port, () => console.log("live at ", port));
